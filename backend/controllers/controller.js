30d816650bd32b8ce125f7be6f8f89bb1ace141e
Pasindu Danthanarayana <65776520+pasindu651@users.noreply.github.com>
2025-01-01 18:05:10 -0500

    test

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -87,7 +87,7 @@ export const logout = async (req, res) => {
 
 export const getUser = (req, res) => {
   console.log("HI");
-  console.log("DEBUG IN getUser, request: " + JSON.stringify(req));
+  console.log("DEBUG IN getUser, request" + req);
   //if the session is successfully created, send user information to frontend
   if (req.session.user) {
     res.json({ success: true, user: req.session.user });
